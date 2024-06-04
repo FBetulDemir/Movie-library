b29b809d0ba307ff54bf9eb623f511c6a3da71f8
FBetulDemir <fatmabetuld@gmail.com>
2024-06-04 21:21:18 +0200

    Created comedy films category

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-let buttonText = document.getElementById("description-button");
+// let buttonText = document.getElementById("description-button");
+
+// function displayText() {
+
+//     let descriptionBox = document.querySelector(".description-box");
+//     buttonText.style.display = "none"
+//     descriptionBox.style.display = "block";
+
+    
+// }
 
 function displayText() {
-
-    let descriptionBox = document.querySelector(".description-box");
-    buttonText.style.display = "none"
-    descriptionBox.style.display = "block";
-
-    
+    alert("Title: Lord of the Rings\nLength: 178 min\nGenre: Fantasy\nDescription: An epic journey to destroy the One Ring.");
 }
-
-function showLess(){
-    
-    buttonText.innerText = "Show less";
-    descriptionBox.style.display = "none";
-}
-
-element.addEventListener("click", displayText);
-
